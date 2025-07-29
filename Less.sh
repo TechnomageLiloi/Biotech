1033b2c0923436f73d6b4fdcb651d401b008cd69
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-07-29 19:26:05 +0300

    [0.00.002] Add LESS compiler SHELL.

---
+++ Less.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+lessc ./Engine/Style.less ./Engine/Style.css
+lessc ./Engine/API/Style.less ./Engine/API/Style.css
+lessc ./Engine/API/Dashboard/Show/Style.less ./Engine/API/Dashboard/Show/Style.css
+lessc ./Engine/API/Exercises/Collection/Style.less ./Engine/API/Exercises/Collection/Style.css
+lessc ./Engine/API/Exercises/Edit/Style.less ./Engine/API/Exercises/Edit/Style.css
